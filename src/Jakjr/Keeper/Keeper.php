97ae5f7ebe2c4bed357e4475db51883ab5ee5630
João Alfredo <joao.alfredo@gmail.com>
2015-07-05 10:00:45 +0000

    to do

--- src/Jakjr/Keeper/Keeper.php
+++ src/Jakjr/Keeper/Keeper.php
@@ -11,6 +11,8 @@ namespace Jakjr\Keeper;
 
 class Keeper {
 
+    //abstrair o contexto
+
     public function keep($context, $inputs)
     {
         foreach($inputs as $key => $value) {
@@ -21,6 +23,11 @@ class Keeper {
     public function get($context, $key)
     {
         return \Session::get("keeper.$context.$key");
+    }
+
+    public function all($context)
+    {
+        return \Session::get("keeper.$context");
     }
 
 }
